6ac78400aa3dc54e6e4f9752c9fafc39aea20a95
Keenan <keenan@sider.review>
2018-10-04 16:49:51 +0900

    add support for CommonJS

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-export const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
-export const split = (string, token) => string.split(token)
-export const uniq = array => array.filter((element, index, array) => index === array.indexOf(element))
-export const zip = (keys, ...arrays) =>
-  arrays[0].map((array, entityIndex) =>
-    keys.reduce((zipped, key, keyIndex) =>
-      Object.assign(zipped, { [key]: arrays[keyIndex][entityIndex] }), {}))
+const esmRequire = require('esm')(module)
+module.exports = esmRequire('./main.js')
