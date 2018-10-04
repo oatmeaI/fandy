05d55cab5447c01b8419ba81dbed55d8ef9ba2a0
Keenan <keenan@sider.review>
2018-10-04 16:31:01 +0900

    codestyle

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-import test from 'ava';
-import {pipe, split, uniq, zip} from './index'
+import test from 'ava'
+import { pipe, split, uniq, zip } from './index'
 
 test('pipe', test => {
   const first = x => x + 1
   const second = x => x * 2
-  
+
   const result = pipe(first, second)(1)
   test.is(result, 4)
-});
+})
 
 test('split', test => {
   const string = 'I-am-a-string'
   const result = split(string, '-')
   test.deepEqual(result, ['I', 'am', 'a', 'string'])
-});
+})
 
 test('uniq', test => {
   const array = ['Bob', 'James', 'Bob']
   const result = uniq(array)
   test.deepEqual(result, ['Bob', 'James'])
-});
+})
 
 test('zip', test => {
   const firstNames = ['Bob', 'James', 'Tim']
@@ -30,8 +30,8 @@ test('zip', test => {
 
   const result = zip(keys, firstNames, lastNames, cities)
   test.deepEqual(result, [
-    {firstName: 'Bob', lastName: 'Builder', currentCity: 'London'},
-    {firstName: 'James', lastName: 'Rocket', currentCity: 'Paris'},
-    {firstName: 'Tim', lastName: 'Tiny', currentCity: undefined},
+    { firstName: 'Bob', lastName: 'Builder', currentCity: 'London' },
+    { firstName: 'James', lastName: 'Rocket', currentCity: 'Paris' },
+    { firstName: 'Tim', lastName: 'Tiny', currentCity: undefined }
   ])
-});
+})
